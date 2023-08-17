c60df9a97c9c22ada5aa23858f94bb830397b54a
Gabriel Dell <gabrielc1990poker@gmail.com>
2023-08-17 10:12:41 -0400

    Decimal Error Guia

--- app/Http/Controllers/Util/Xml/dos_uno/GuiaRemision_2_1.php
+++ app/Http/Controllers/Util/Xml/dos_uno/GuiaRemision_2_1.php
@@ -122,7 +122,7 @@ class GuiaRemision_2_1 extends XmlHelperNew
           [ "id_item_guia"  , $i ],
           [ "id_item_documento" , $i ],          
           [ "unidad" , $item->DetUnid ],          
-          [ "cantidad" , (int) $item->Detcant ],
+          [ "cantidad" , $item->Detcant ],
           [ "nombre_producto"   , $item->DetNomb . ' ' . $item->DetDeta ],
           [ "id_producto", $item->DetCodi ],
         ] , $this->item_base , false 
