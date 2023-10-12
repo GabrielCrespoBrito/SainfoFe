634ad6c5704e4ac9472edb49de1279a2f80ca5b9
Gabriel Dell <gabrielc1990poker@gmail.com>
2023-10-12 12:23:32 -0400

    clientes route error

--- routes/routes_web/sire.php
+++ routes/routes_web/sire.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('sire')->group(function(){
 
-	Route::get('/', 'SireController@index')->name('clientes.index');
+	Route::get('/', 'SireController@index')->name('sire.index');
 
 });
