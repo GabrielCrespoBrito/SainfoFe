92fe3f877b90273ab04b1824b28b41f8dd1ad767
Gabriel Dell <gabrielc1990poker@gmail.com>
2024-02-08 17:53:47 -0400

    e

--- resources/views/reportes/vendedor/partials/zonas/table_head.blade.php
+++ resources/views/reportes/vendedor/partials/zonas/table_head.blade.php
@@ -1,8 +1,8 @@
 <thead class="no-font-weight font-size-7">
   <tr>
-    <td> Codigo </td>
-    <td> Tipo Doc. </td>
-    <td> Documento </td>
+    <td style="padding-right:5px; padding-left:5px;"> Codigo </td>
+    <td style="padding-right:5px; padding-left:5px;"> Tipo Doc. </td>
+    <td style="padding-right:5px; padding-left:5px;"> Documento </td>
     <td width="40%"> Razon Social </td>
     <td width="20%"> Zona </td>
     <td width="25%"> Vendedor </td>
