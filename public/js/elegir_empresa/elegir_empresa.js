931298a2c216ee29846ca9cea9e8c56e55f7c884
Gabriel Dell <gabrielc1990poker@gmail.com>
2024-03-27 16:26:43 -0400

    elegir empresa

--- public/js/elegir_empresa/elegir_empresa.js
+++ public/js/elegir_empresa/elegir_empresa.js
@@ -1,53 +1,13 @@
 $(function () {
-  
   let $formEle = $("form#elegirEmpresaPeriodo");
 
-  function changeEmpresaByUrl() {
+  $formEle.on('submit', function (e) {
+    $("[type=submit]").prop('disabled', true);
+  });
 
-    let ruc = $("[name=empresa] option:selected").attr('data-ruc');
+  $formEle.find("[name=empresa]").on('change', function (e) {
 
-    const urlInstance = new URL(location.href);
-
-    if ( urlInstance.host.includes('.') == false ) {
-      return;
-    }
-
-    let rucUrl = urlInstance.host.split('.')[0];
-    
-    if(ruc != rucUrl){
-       $(`[name=empresa] option[data-ruc=${rucUrl}]`).prop('selected', true);
-    }
-  }
-
-  function changeUrl() {
-
-    let ruc = $("[name=empresa] option:selected").attr('data-ruc');
-    
-    const urlInstance = new URL(location.href);
-    
-    // console.log(ruc, urlInstance);
-
-    if(urlInstance.host.includes(ruc)){
-      return;
-    }
-    
-    let host = urlInstance.host;
-
-    if(host.includes('.') ){
-      host = host.split('.')[1];
-    }
-
-    console.log(
-      `${urlInstance.protocol}//${ruc}.${host}${urlInstance.pathname}`
-
-    )
-
-    location.href = `${urlInstance.protocol}//${ruc}.${host}${urlInstance.pathname}`;
-  }
-
-  function setPeriodo()
-  {
-    let dataPeriodos = $("[name=empresa] option:selected").attr('data-periodo').split(",");
+    let dataPeriodos = $("option:selected", this).attr('data-periodo').split(",");
     let selectPeriodo = $("[name=periodo]", $formEle);
     selectPeriodo.empty();
     for (let i = 0; i < dataPeriodos.length; i++) {
@@ -64,18 +24,6 @@ $(function () {
       selectPeriodo.append(option);
     }
 
-  }
-
-
-  $formEle.on('submit', function (e) {
-    $("[type=submit]").prop('disabled', true);
   });
-
-  
-  $formEle.find("[name=empresa]").on('change', setPeriodo);
-  $formEle.find("[name=empresa]").on('change', changeUrl );
-  
-  changeEmpresaByUrl();
-  changeUrl();
 
 });
