062d6818cc34e0c92cf9ee4183485017e6065854
Gabriel Dell <gabrielc1990poker@gmail.com>
2024-05-14 16:28:34 -0400

    e

--- resources/views/reportes/ventas_tipopago/partials/table.blade.php
+++ resources/views/reportes/ventas_tipopago/partials/table.blade.php
@@ -19,7 +19,7 @@
       <td> {{ $item->VtaOper }} </td>
       <td> {{ $item->PagFech }} </td>
       <td> {{ $item->PagBoch }} </td>
-      <td> {{ $item->cliente->PCNomb }} </td>
+      <td> {{ optional($item->cliente)->PCNomb ?? $item->PCNomb }} </td>
       <td> {{ $item->CtoOper }} </td>
       <td> {{ $item->Bannomb }} </td>
       <td> {{ $item->MonCodi }} </td>
