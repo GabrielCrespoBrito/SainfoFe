ed0a0744c4b6559422f448b2aca082d3e1c4184c
Gabriel Dell <gabrielc1990poker@gmail.com>
2025-10-01 14:09:31 -0400

    Agregar Fecha de Emision

--- resources/views/pdf/documents/default/ticket_cot1.blade.php
+++ resources/views/pdf/documents/default/ticket_cot1.blade.php
@@ -43,6 +43,7 @@
     'class_name' => 'col-12 border-width-1 mt-x5 mb-x5  border-bottom-style-dotted',
     'nombre_campo_nombre' => 'Razón Social:',
     'nombre_campo_class' => 'bold',
+    'fecha_emision' => $fecha_emision_ ?? false,
     'nombre' => $cliente->PCNomb,
     'documento_campo_nombre' => $cliente->getNombreTipoDocumento() . ':',
     'documento_campo_class' => 'bold',
